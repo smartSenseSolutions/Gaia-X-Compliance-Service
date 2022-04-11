a6e22838ceec6f955ccd30093f976d870c267ea3
Moritz Kirstein <mkirstein@protonmail.com>
2022-04-11 18:39:18 +0200

    refactor: change global prefix, add software info to '/'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ParticipantController } from './participant/participant.controller'
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ParticipantController],
   providers: [AppService]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,19 @@ import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { name, version, description } from '../package.json'
+import { RequestMethod } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.setGlobalPrefix('/api')
+  app.setGlobalPrefix('/api/v1', {
+    exclude: [{ path: '/', method: RequestMethod.GET }]
+  })
 
   const config = new DocumentBuilder().setTitle(name).setDescription(description).setVersion(version).build()
 
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api-docs', app, document)
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(3000)
 }
