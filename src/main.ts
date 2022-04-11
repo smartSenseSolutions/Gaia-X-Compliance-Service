fb8aecfa8e607de74d120e813ac22e5302c027f3
Moritz Kirstein <mkirstein@protonmail.com>
2022-04-11 18:31:15 +0200

    deps: add swagger as dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
+import { name, version, description } from '../package.json'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('/api')
+
+  const config = new DocumentBuilder().setTitle(name).setDescription(description).setVersion(version).build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api-docs', app, document)
+
   await app.listen(3000)
 }
 bootstrap()
