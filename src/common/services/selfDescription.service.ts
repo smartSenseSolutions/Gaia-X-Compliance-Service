79fe353b6230aec62978ad63759372f3c8acb8c0
Albert Peci <albert.peci@posteo.de>
2022-06-16 22:35:14 +0000

    Development

--- src/common/services/selfDescription.service.ts
+++ src/common/services/selfDescription.service.ts
@@ -21,8 +21,8 @@ export class SelfDescriptionService {
   }
   // TODO extract to common types
   static readonly TYPES = {
-    PARTICIPANT: 'gx-participant:LegalPerson',
-    SERVICE_OFFERING: 'gx-service-offering-experimental:ServiceOfferingExperimental'
+    PARTICIPANT: 'LegalPerson',
+    SERVICE_OFFERING: 'ServiceOfferingExperimental'
   }
   static readonly SHAPE_PATH_PARTICIPANT = '/shapes/v1/participant.ttl'
 
@@ -32,7 +32,7 @@ export class SelfDescriptionService {
     private readonly participantContentService: ParticipantContentValidationService,
     private readonly serviceOfferingContentValidationService: ServiceOfferingContentValidationService,
     private readonly proofService: ProofService
-  ) { }
+  ) {}
 
   public async validate(signedSelfDescription: SignedSelfDescriptionDto, isComplianceCredentialCheck?: boolean): Promise<ValidationResultDto> {
     const { selfDescriptionCredential: selfDescription, raw, complianceCredential, proof } = signedSelfDescription
@@ -42,6 +42,7 @@ export class SelfDescriptionService {
         : selfDescription['@type']
 
       const shapePath = this.getShapePath(type)
+
       if (!shapePath) {
         throw new BadRequestException('Provided Type does not exist for Self Descriptions')
       }
@@ -96,7 +97,7 @@ export class SelfDescriptionService {
 
     try {
       const selfDescriptionDataset = await this.shaclService.loadFromJsonLD(raw)
-      const type: string = selfDescription['@type'] // selfDescription['@type'].find(t => t !== 'VerifiableCredential')
+      const type: string = selfDescription['@type'].find(t => t !== 'VerifiableCredential') // selfDescription['@type'] //
 
       const shapePath = this.getShapePath(type)
 
@@ -144,9 +145,7 @@ export class SelfDescriptionService {
     if (type === SelfDescriptionService.TYPES.PARTICIPANT) {
       content = await this.participantContentService.validate(selfDescription)
     } else {
-      const result: ValidationResultDto = await this.validateProvidedByParticipantSelfDescriptions(
-        (selfDescription as ServiceOfferingSelfDescriptionDto)['gx-service-offering:providedBy']
-      )
+      const result: ValidationResultDto = await this.validateProvidedByParticipantSelfDescriptions(selfDescription.providedBy)
 
       content = await this.serviceOfferingContentValidationService.validate(selfDescription as ServiceOfferingSelfDescriptionDto, result)
     }
