2f529cd3dbef0bafa07aca15f05366c2cc0cfd39
Henry Faure-Geors <henry.faure-geors@atos.net>
2023-02-20 15:56:36 +0000

    Feat/integration-test + Fix: test data / /api/verify method

--- test/test-2210.js
+++ test/test-2210.js
@@ -27,7 +27,8 @@ let checks =
         name:"testServiceOfferingRulesOK" ,
         url :  'https://compliance.lab.gaia-x.eu/api/service-offering/verify/raw' ,  
         testfile : './datas/2210/serviceOffering-ok.json',
-        testResult : function (body) {  return body.conforms == true },
+        testResult : function (body) {  
+            return body.conforms == true },
         type: "post"
     },
     {
@@ -43,7 +44,73 @@ let checks =
         testfile : './datas/2210/serviceOffering-ko-HttpCode.json',
         testResult : function (body) {  return body.message == "Participant SD not found" },
         type: "post"
-    }
+    }, 
+    { 
+        name:"testParticipantRulesOKUniform" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/verify' ,  
+        testfile : './datas/2210/participant-ok.json',
+        testResult : function (body) {  return body.conforms == true },
+        type: "post"
+    },
+    {
+        name:"testServiceOfferingRulesOKUniform" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/verify' ,  
+        testfile : './datas/2210/serviceOffering-ok.json',
+        testResult : function (body) {  return body.conforms == true },
+        type: "post"
+    },
+    {
+        name:"testServiceOfferingRulesKO-HttpCodeUniform" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/verify' ,  
+        testfile : './datas/2210/serviceOffering-ko-HttpCode.json',
+        testResult : function (body) {  return body.message == "Participant SD not found" },
+        type: "post"
+    }, 
+    {
+        name:"testServiceOfferingRulesKO-CheckDidUniform" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/verify' ,  
+        testfile : './datas/2210/serviceOffering-ko-CheckDid.json',
+        testResult : function (body) {  return body.statusCode == 409 },
+        type: "post"
+    },
+    { 
+        name:"testParticipantRulesKO-CheckDIDUniform" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/verify' ,  
+        testfile : './datas/2210/participant-ko-checkDid.json',
+        testResult : function (body) {  return body.message.conforms == false },
+        type: "post"
+    },
+    { 
+        name:"testVC-Issuance-Participant-OK" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/vc-issuance' ,  
+        testfile : './datas/2210/participant-ok-sd.json',
+        testResult : function (body) {  
+            return !body.complianceCredential  == false },
+        type: "post"
+    },
+    { 
+        name:"testVC-Issuance-Participant-KO-did" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/vc-issuance' ,  
+        testfile : './datas/2210/participant-ko-did-sd.json',
+        testResult : function (body) {  
+            return body.message.conforms == false},
+        type: "post"
+    },
+    { 
+        name:"testVC-Issuance-Service-offering-OK" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/vc-issuance' ,  
+        testfile : './datas/2210/service-offering-ok-sd.json',
+        testResult : function (body) {  return !body.complianceCredential  == false },
+        type: "post"
+    },
+    { 
+        name:"testVC-Issuance-Service-offering-KO-http" ,
+        url :  'https://compliance.lab.gaia-x.eu/api/vc-issuance' ,  
+        testfile : './datas/2210/service-offering-ko-http-sd.json',
+        testResult : function (body) {  
+            return body.message.conforms == false},
+        type: "post"
+    },
 ];
 
 export default checks;
