93dc60bb8ad3331649ef494cef9787fc65b1c03f
Henry Faure-Geors <henry.faure-geors@atos.net>
2023-03-01 16:49:56 +0000

    Feat:  unit test fixing + Data export fixing

--- src/common/services/shacl.service.ts
+++ src/common/services/shacl.service.ts
@@ -1,17 +1,38 @@
 import { HttpService } from '@nestjs/axios'
-import { ConflictException, Injectable } from '@nestjs/common'
+import { ConflictException, Injectable, NotFoundException, Logger } from '@nestjs/common'
 import { Readable } from 'stream'
-import { ValidationResult } from '../dto/validation-result.dto'
 import DatasetExt from 'rdf-ext/lib/Dataset'
 import Parser from '@rdfjs/parser-n3'
 import ParserJsonLD from '@rdfjs/parser-jsonld'
 import rdf from 'rdf-ext'
+import { EXPECTED_PARTICIPANT_CONTEXT_TYPE, EXPECTED_SERVICE_OFFERING_CONTEXT_TYPE } from '../constants'
 import SHACLValidator from 'rdf-validate-shacl'
+import { SelfDescriptionTypes } from '../enums'
+import {
+  CredentialSubjectDto,
+  Schema_caching,
+  ValidationResult,
+  VerifiableCredentialDto,
+} from '../dto'
+import { NotFoundError } from 'rxjs'
+
+const expectedContexts = {
+  [SelfDescriptionTypes.PARTICIPANT]: EXPECTED_PARTICIPANT_CONTEXT_TYPE,
+  [SelfDescriptionTypes.SERVICE_OFFERING]: EXPECTED_SERVICE_OFFERING_CONTEXT_TYPE
+}
+const cache: Schema_caching = {
+  LegalPerson: {},
+  ServiceOfferingExperimental: {}
+}
 
 @Injectable()
 export class ShaclService {
   constructor(private readonly httpService: HttpService) {}
-
+  private readonly logger = new Logger(ShaclService.name)
+  static readonly SHAPE_PATHS = {
+    PARTICIPANT: 'participant',
+    SERVICE_OFFERING: 'serviceoffering'
+  }
   async validate(shapes: DatasetExt, data: DatasetExt): Promise<ValidationResult> {
     const validator = new SHACLValidator(shapes, { factory: rdf as any })
     const report = await validator.validate(data)
@@ -55,6 +76,18 @@ export class ShaclService {
     }
   }
 
+  async loadShaclFromUrl(type:string): Promise<DatasetExt> {
+    try {
+      const url = process.env.REGISTRY_URL || "https://registry.lab.gaia-x.eu"
+      const response= await (await this.httpService.get(`${url}/api/trusted-shape-registry/v1/shapes/${type}`).toPromise()).data   
+      return this.isJsonString(response.data) ? this.loadFromJsonLD(response.data) : this.loadFromTurtle(response.data)
+    } catch (error) {
+      this.logger.error(`${error}, Url used to fetch shapes: ${process.env.REGISTRY_URL}/api/trusted-shape-registry/v1/shapes/${type}`)
+      throw new ConflictException(error)
+      
+    }
+  }
+
   async loadFromUrl(url: string): Promise<DatasetExt> {
     try {
       const response = await this.httpService
@@ -63,11 +96,11 @@ export class ShaclService {
           transformResponse: r => r
         })
         .toPromise()
-
+      
       return this.isJsonString(response.data) ? this.loadFromJsonLD(response.data) : this.loadFromTurtle(response.data)
     } catch (error) {
       console.error(error)
-      throw new ConflictException('SHACL file cannot be loaded from provided url.')
+      throw new ConflictException("Cannot load TTL file for url ", url)
     }
   }
 
@@ -87,5 +120,59 @@ export class ShaclService {
     }
 
     return true
+  }
+
+  
+  public async getShaclShape(link: string): Promise<DatasetExt> {
+    return await this.loadShaclFromUrl(link)
+  }
+
+  public async ShapeVerification(
+    rawCredentialSubject: string,
+    type: string,
+  ): Promise<ValidationResult> {
+    try {
+      const rawPrepared = {
+        ...JSON.parse(rawCredentialSubject),
+        ...expectedContexts[type]
+      }
+      const selfDescriptionDataset: DatasetExt = await this.loadFromJsonLD(JSON.stringify(rawPrepared))
+      if (this.Cache_check(type) == true) {
+        const shape: ValidationResult = await this.validate(cache[type].shape, selfDescriptionDataset)
+        return shape
+      } else {
+        try {
+          const schema = await this.getShaclShape(this.getShapePath(type))
+          cache[type].shape = schema
+          const shape: ValidationResult = await this.validate(schema, selfDescriptionDataset)
+          return shape
+        }
+        catch (e) {
+          return {
+            conforms:false,
+            results:[e]
+          }
+        }
+      }
+    } catch (e) {
+      throw e
+    }
+  }
+
+  private Cache_check(type: string): boolean {
+    let cached = false
+    if (cache[type].shape) {
+      cached = true
+    }
+    return cached
+  }
+
+  private getShapePath(type: string): string | undefined {
+    const shapePathType = {
+      [SelfDescriptionTypes.PARTICIPANT]: 'PARTICIPANT',
+      [SelfDescriptionTypes.SERVICE_OFFERING]: 'SERVICE_OFFERING'
+    }
+
+    return ShaclService.SHAPE_PATHS[shapePathType[type]] || undefined
   }
 }
