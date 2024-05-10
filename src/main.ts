ca91f658b90d2068cade9132c1e29bfddc742df3
Ewann Gavard <ewann.gavard@gaia-x.eu>
2024-05-10 08:09:58 +0000

    fix: remove sgx quote from normal flow
    Signed-off-by: egavard <ewann@bakeup.io>

--- src/main.ts
+++ src/main.ts
@@ -22,23 +22,6 @@ async function bootstrap() {
   setupSwagger(app)
   app.enableCors()
   app.enableShutdownHooks()
-  try {
-    const fd = fs.openSync('/dev/attestation/user_report_data', 'r+')
-    fs.writeSync(fd, 'gx-compliance')
-    const fdQuote = fs.openSync('/dev/attestation/report', 'r')
-    const quote = fs.readFileSync(fdQuote)
-    console.log('==============\n')
-    console.log(quote.toString('base64'))
-    console.log('\n==============\n')
-    /*
-        console.log(quote.toString('ascii'))
-        console.log(quote.toString('base64'))
-        console.log(quote.toString('hex'))
-    */
-  } catch (error) {
-    console.log('SGX Quote unavailable')
-  }
-  /*await console.log(got.get('https://registry.lab.gaia-x.eu/v1/docs').json())*/
   await app.listen(process.env.PORT || 3000)
 }
 
