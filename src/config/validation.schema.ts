e1a87f20c36995de7189e1998a196ad627cf7e08
Vincent Kelleher <vincent.kelleher@gaia-x.eu>
2024-05-17 16:41:29 +0200

    fix(LAB-629): remove unused environment variables
    This removes SD_STORAGE_BASE_URL & SD_STORAGE_API_KEY
    
    Signed-off-by: Vincent Kelleher <vincent.kelleher@gaia-x.eu>

--- src/config/validation.schema.ts
+++ src/config/validation.schema.ts
@@ -3,8 +3,6 @@ import Joi from 'joi'
 export default Joi.object({
   PORT: Joi.alternatives().try(Joi.string(), Joi.number()).default(3000),
   REGISTRY_URL: Joi.string().required(),
-  SD_STORAGE_BASE_URL: Joi.string(),
-  SD_STORAGE_API_KEY: Joi.string(),
   privateKey: Joi.string().required(),
   X509_CERTIFICATE: Joi.string().optional()
 })
